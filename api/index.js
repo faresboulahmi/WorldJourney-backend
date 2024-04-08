193c2b92a2cce30101a03689a75df6fce89222b4
fares <132559795+faresboulahmi@users.noreply.github.com>
2024-04-08 18:28:06 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,8 +7,10 @@ import tourRouter from "./routes/tour.route.js";
 import reviewsRouter from "./routes/reviews.route.js";
 import adminRouter from "./routes/admin.route.js";
 import cookieParser from "cookie-parser";
+import cors from "cors"; 
+import { verifyToken, setCookieOptions } from "./utils/verifyUser.js"; // Import verifyToken middleware
+
 import path from "path";
-import cors from "cors";
 dotenv.config();
 
 mongoose
@@ -22,29 +24,28 @@ mongoose
 
 const __dirname = path.resolve();
 
-
-
 const app = express();
 
 app.use(express.json());
 
 app.use(cookieParser());
-const cookieOptions = {
-  sameSite: 'None', 
-  secure: true, 
-  httpOnly: true 
-};
-res.cookie('access_token', token, cookieOptions);
 
+
+
+// Use CORS middleware to allow requests from http://localhost:5173
 app.use(cors({
-  origin: 'http://localhost:5173'
+  origin: 'http://localhost:5173',
+  credentials: true
 }));
+
+app.use(setCookieOptions);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
 
-app.use("/api/user", userRouter);
+
+app.use("/api/user", verifyToken ,  userRouter); 
 app.use("/api/auth", authRouter);
 app.use("/api/tour", tourRouter);
 app.use("/api/reviews", reviewsRouter);
