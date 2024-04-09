fe5a7a46e3633d8c1906fbdff479411b308e5b45
fares <132559795+faresboulahmi@users.noreply.github.com>
2024-04-09 21:22:03 +0100

    Update admin.route.js

--- api/routes/admin.route.js
+++ api/routes/admin.route.js
@@ -1,12 +1,12 @@
 import express from 'express' ;
 import { deleteUser, getUser, updateUser } from '../controllers/admin.controller.js';
-import { verifyToken } from '../utils/verifyUser.js';
+
 
 
 const router = express.Router();
 
 router.get('/get' , getUser);
-router.delete('/delete/:id' , verifyToken , deleteUser);
+router.delete('/delete/:id' , deleteUser);
 router.post('/update/:id' , updateUser);
 
 export default router
