d264ec76a4c76c0d93ae33c4b1f323eb2f5a2482
fares <132559795+faresboulahmi@users.noreply.github.com>
2024-04-16 08:15:56 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -31,8 +31,7 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    // origin: "https://worldjourney.vercel.app",
-    origin: "http://localhost:5173",
+    origin: "https://worldjourney.vercel.app",
     credentials: true,
 
   })
